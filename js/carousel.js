6e5a2c05f4f0cedf8f5b88bc11ddca0fd0800f6a
Fon <1090737707@qq.com>
2017-05-13 00:50:55 +0800

    carousel ver 1.0

---
+++ js/carousel.js
@@ -0,0 +1,73 @@
+var list = document.getElementById("image-list")
+var pre = document.getElementById("pre-button")
+var next = document.getElementById("next-button")
+var carouselContainer = document.getElementById("carousel-container")
+var buttons = document.getElementById("buttons").getElementsByTagName("span")
+var timer
+var current = 0
+var theLastPositon = -2400
+var pictureWidth = 600
+var animateDistance = function (distance) {
+    var newLeft = parseInt(list.offsetLeft) + distance
+    list.style.left = newLeft + "px"
+    if (newLeft < theLastPositon) {
+        list.style.left = 0 + "px"
+    }
+    if (newLeft > 0) {
+        list.style.left = theLastPositon + "px"
+    }
+}
+
+var buttonShow = function(){
+    for(let i = 0; i <buttons.length; i ++){
+        if(buttons[i].className == "on"){
+            buttons[i].className = ""
+        }
+    }
+    buttons[current].className = "on"
+}
+
+for(let i = 0; i < buttons.length; i ++){
+    
+}
+
+pre.addEventListener("click", function () {
+    current -= 1
+    if(current < 0){
+        current = 4
+    }
+    buttonShow()
+    animateDistance(pictureWidth)
+})
+
+next.addEventListener("click", function () {
+    current += 1
+    if(current > 4){
+        current = 0
+    }
+    buttonShow()
+    animateDistance(0-pictureWidth)
+})
+
+var play = function () {
+    timer = setInterval(function () {
+        next.click()
+    }, 3000)
+}
+
+play()
+
+var stop = function(){
+    clearInterval(timer)
+}
+
+carouselContainer.onmouseover = function(){
+    pre.style.opacity = 1
+    next.style.opacity = 1
+    stop()
+}
+carouselContainer.onmouseout = function(){
+    pre.style.opacity = 0.4
+    next.style.opacity = 0.4
+    play()
+}
