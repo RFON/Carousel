cfd3c1bccc99101eab2889a3d123bd58a69385df
Fon <1090737707@qq.com>
2017-05-14 02:21:18 +0800

    carousel ver 1.0.1

--- js/carousel.js
+++ js/carousel.js
@@ -5,62 +5,73 @@ var carouselContainer = document.getElementById("carousel-container")
 var buttons = document.getElementById("buttons").getElementsByTagName("span")
 var timer
 var current = 0
-var theLastPositon = -2400
+var totalWidth = -2400
 var pictureWidth = 600
 
-var animate = function(element, className){
-    element.classList.add(className)
-    element.addEventListener('animationend',function(){
-        element.classList.remove(className)
-    },false)
-    console.log(list.style.left)
-}
+// var animate = function(element, className,long){
+//     element.classList.add(className)
+//     element.addEventListener('animationend',function(){
+//         element.classList.remove(className)
+//     },false)
+//     animateDistance(long)
+//     console.log(list.style.left)
+// }
 
+// 偏移距离
 var animateDistance = function (distance) {
-    console.log(list.style.left)
     var newLeft = parseInt(list.offsetLeft) + distance
     list.style.left = newLeft + "px"
-    if (newLeft < theLastPositon) {
+    if (newLeft < totalWidth) {
         list.style.left = 0 + "px"
     }
     if (newLeft > 0) {
-        list.style.left = theLastPositon + "px"
+        list.style.left = totalWidth + "px"
     }
 }
 
-var buttonShow = function(){
-    for(let i = 0; i <buttons.length; i ++){
-        if(buttons[i].className == "on"){
+// 小圆点变色
+var buttonShow = function () {
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].className == "on") {
             buttons[i].className = ""
         }
     }
     buttons[current].className = "on"
 }
 
-for(let i = 0; i < buttons.length; i ++){
-    
-}
-
+// 左右切换图片按钮
 pre.addEventListener("click", function () {
     current -= 1
-    if(current < 0){
+    if (current < 0) {
         current = 4
     }
     buttonShow()
-    animate(list,"translate-left",pictureWidth)
+    // animate(list,"translate-left",pictureWidth)
     animateDistance(pictureWidth)
 })
 
 next.addEventListener("click", function () {
     current += 1
-    if(current > 4){
+    if (current > 4) {
         current = 0
     }
     buttonShow()
-    animate(list,"translate-left",pictureWidth)
-    animateDistance(0-pictureWidth)
+    // animate(list,"translate-left",0-pictureWidth)
+    animateDistance(0 - pictureWidth)
 })
 
+// 小圆点按钮点击切换图片
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].index = i
+    buttons[i].addEventListener('click', function () {
+        this.className = "on"
+        buttons[current].className = ""
+        list.style.left = (0 - pictureWidth) * this.index + "px"
+        current = this.index
+    })
+}
+
+// 循环播放
 var play = function () {
     timer = setInterval(function () {
         next.click()
@@ -69,16 +80,20 @@ var play = function () {
 
 play()
 
-var stop = function(){
+// 停止播放
+var stop = function () {
     clearInterval(timer)
 }
 
-carouselContainer.onmouseover = function(){
+// 鼠标停留在轮播图上停止播放
+carouselContainer.onmouseover = function () {
     pre.style.opacity = 1
     next.style.opacity = 1
     stop()
 }
-carouselContainer.onmouseout = function(){
+
+// 鼠标移开继续播放
+carouselContainer.onmouseout = function () {
     pre.style.opacity = 0.4
     next.style.opacity = 0.4
     play()
